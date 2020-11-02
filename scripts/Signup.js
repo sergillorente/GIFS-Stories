11d1f007aac32112745472c3dc1fe3bd07eca2a9
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2020-11-02 11:21:51 +0100

    Sign up file (JS part) initialized

--- scripts/Signup.js
+++ scripts/Signup.js
@@ -1,1 +1,108 @@
 'use strict';
+
+class Signup {
+    constructor() {
+
+      this.nameInput = document.querySelector("#name");
+      this.emailInput = document.querySelector("#email");
+      this.passwordInput = document.querySelector("#password");
+      this.repeatPasswordInput = document.querySelector("#repeat-password");
+  
+      this.buttonInput = document.querySelector("#signup-button");
+      this.errorsWrapper = document.querySelector(".message-container");
+    }
+  
+    handleEmailInput = (event) => {
+      const emailInput = event.target;
+      const email = emailInput.value;
+  
+      validator.validateValidEmail(email);
+      validator.validateUniqueEmail(email);
+  
+      this.setErrorMessages();
+    };
+  
+  
+  
+  
+    handlePasswordInput = (event) => {
+      const passwordInput = event.target;
+      const repeatPasswordInput = this.repeatPasswordInput;
+  
+      const password = passwordInput.value;
+      const repeatPassword = repeatPasswordInput.value;
+  
+      validator.validatePassword(password);
+      validator.validateRepeatPassword(password, repeatPassword);
+  
+      this.setErrorMessages();
+    };
+  
+  
+    handleRepeatPasswordInput = (event) => {
+      const passwordInput = event.target;
+      const repeatPasswordInput = this.repeatPasswordInput;
+  
+      const password = passwordInput.value;
+      const repeatPassword = repeatPasswordInput.value;
+  
+      validator.validatePassword(password);
+      validator.validateRepeatPassword(password, repeatPassword);
+  
+      this.setErrorMessages();
+    };
+  
+  
+  
+    setErrorMessages = () => {
+  
+      this.errorsWrapper.innerHTML = "";
+  
+      const errorsObj = validator.getErrors();
+  
+      
+      const errorStringsArr = Object.values( errorsObj );
+  
+      errorStringsArr.forEach( (str) => {
+        const p = document.createElement('p');
+        p.textContent = str;
+  
+        this.errorsWrapper.appendChild(p);
+      })
+    }
+  
+  
+  
+  
+    saveData = (event) => {
+  
+      event.preventDefault();
+  
+      const name = this.nameInput.value;
+      const email = this.emailInput.value;
+      const password = this.passwordInput.value;
+  
+      const newUser = new User(name, email, password);
+  
+      db.saveNewUser(newUser);
+  
+      this.nameInput.value = "";
+      this.emailInput.value = "";
+      this.passwordInput.value = "";
+    };
+  
+    addListeners = () => {
+      this.emailInput.addEventListener("input", this.handleEmailInput);
+      this.passwordInput.addEventListener("input", this.handlePasswordInput);
+      this.repeatPasswordInput.addEventListener("input", this.handleRepeatPasswordInput);
+      this.buttonInput.addEventListener("click", this.saveData);
+    }
+  
+  }
+  
+  
+  const signup = new Signup();
+  
+  
+  
+  window.addEventListener('load', signup.addListeners )
