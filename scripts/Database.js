31e8d6480d96eecc1ef857804ac110189d9ec237
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2020-11-02 11:42:26 +0100

    Database file initialized (JS part)

--- scripts/Database.js
+++ scripts/Database.js
@@ -1,1 +1,27 @@
-'use strict'
+'use strict';
+
+class Database {
+    getAllUsers = () => {
+      const usersStr = localStorage.getItem("users");
+      const usersArr = JSON.parse(usersStr); 
+  
+      if (usersStr === null) {
+        return [];
+      } else {
+        return usersArr;
+      }
+    };
+
+  
+    saveNewUser = (newUser) => {
+      const usersArr = this.getAllUsers();
+  
+      const updatedUsersArr = usersArr.push(newUser);
+  
+      const updatedUsersStr = JSON.stringify(updatedUsersArr);
+      localStorage.setItem("users", updatedUsersStr);
+    };
+  }
+
+  const db = new Database();
+  
